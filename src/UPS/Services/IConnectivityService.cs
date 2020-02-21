5e371a0f7d12cde1494917d616b3f0cbd635999b
Juan Garces <juanp3053@gmail.com>
2020-02-21 11:16:32 -0600

    more setup and adding versioning

---
+++ src/UPS/Services/IConnectivityService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPS.Services
+{
+    public interface IConnectivityService
+    {
+        Task<bool> HasConnectivity();
+        Task<bool> HasConnectivityTo(Uri uri);
+        Task<bool> HasConnectivityTo(string uri);
+
+    }
+}
