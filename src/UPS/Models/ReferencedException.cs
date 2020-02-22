19feddeeeac880881b7cd952c316755db7051f8f
Juan Garces <juanp3053@gmail.com>
2020-02-22 14:47:32 -0600

    working on fixing build

--- src/UPS/Models/ReferencedException.cs
+++ src/UPS/Models/ReferencedException.cs
@@ -4,9 +4,19 @@ using System.Text;
 
 namespace UPS.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ReferencedException
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public Guid guid { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public Exception exception { get; set; }
     }
 }
